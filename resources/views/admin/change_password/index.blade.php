56c62ce3a34dbf3ebc9eb536b73dbc719e6bf38e
nibhashlbsnaa <nibhash.mishra@awzpact.com>
2024-11-07 12:42:11 +0530

    ui chnages of menu section

--- resources/views/admin/change_password/index.blade.php
+++ resources/views/admin/change_password/index.blade.php
@@ -1,59 +1,96 @@
-@extends('layouts.app')
 @extends('admin.layouts.master')
 
 @section('title', 'Admin Dashboard')
 
 @section('content')
-    <div class="container">
-        <h3 class="editprofile">Change Password</h3>
-        <!-- Check for success message -->
-        @if (session('success'))
-            <div id="success-alert" class="alert alert-success">
-                {{ session('success') }}
-            </div>
-        @endif
+<div class="d-sm-flex text-center justify-content-between align-items-center mb-4">
+    <h3 class="mb-sm-0 mb-1 fs-18">Change Password</h3>
+    <ul class="ps-0 mb-0 list-unstyled d-flex justify-content-center">
+        <li>
+            <a href="{{route('admin.index')}}" class="text-decoration-none">
+                <i class="ri-home-2-line" style="position: relative; top: -1px;"></i>
+                <span>Dashboard</span>
+            </a>
+        </li>
+        <li>
+            <span class="fw-semibold fs-14 heading-font text-dark dot ms-2">Change Password</span>
+        </li>
+    </ul>
+</div>
+<div class="row justify-content-center">
+    <div class="col-xxl-9">
+        <div class="card bg-white border-0 rounded-10 mb-4">
+            <div class="card-body p-4">
+            @if (session('success'))
+<div id="success-alert" class="alert alert-success">
+    {{ session('success') }}
+</div>
+@endif
 
-        <!-- Check for error messages -->
-        @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-        <form id="changepass" name="changepass" method="post" action="{{ route('update_password') }}" autocomplete="off">
-            @csrf
-            <div class="form-group">
-                <label for="txtpwd">Enter Old Password:</label>
-                <input type="password" name="old_password" class="form-control" id="txtpwd" required>
-                @error('old_password')
-                    <div class="text-danger">{{ $message }}</div>
-                @enderror
-            </div>
-
-            <div class="form-group">
-                <label for="txtnpwd">Enter New Password:</label>
-                <input type="password" name="new_password" class="form-control" id="txtnpwd" required>
-                <small>Password must be 8 characters, with at least 1 number, 1 lowercase, and 1 uppercase letter.</small>
-                @error('new_password')
-                    <div class="text-danger">{{ $message }}</div>
-                @enderror
-            </div>
-
-            <div class="form-group">
-                <label for="txtcpwd">Confirm Password:</label>
-                <input type="password" name="confirm_password" class="form-control" id="txtcpwd" required>
-                @error('confirm_password')
-                    <div class="text-danger">{{ $message }}</div>
-                @enderror
-            </div>
-
-            <button type="submit" class="btn btn-primary">Update</button>
-        </form>
-
-        <!-- JavaScript to remove the success message after 10 seconds -->
+<!-- Check for error messages -->
+@if ($errors->any())
+<div class="alert alert-danger">
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
+                <form id="changepass" name="changepass" method="post" action="{{ route('update_password') }}" autocomplete="off">
+                    @csrf
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="form-group mb-4">
+                                <label class="label" for="txtpwd">Enter Old Password</label>
+                                <div class="form-group">
+                                    <div class="password-wrapper position-relative">
+                                        <input type="password" id="txtpwd" class="form-control h-58 text-dark" name="old_password">
+                                        <i style="color: #A9A9C8; font-size: 16px; right: 15px !important;" class="ri-eye-off-line password-toggle-icon translate-middle-y top-50 end-0 position-absolute" aria-hidden="true"></i>
+                                        @error('old_password')
+                                        <div class="text-danger">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-group mb-4">
+                                <label class="label" for="txtnpwd">Enter New Password</label>
+                                <div class="form-group">
+                                    <div class="password-wrapper position-relative">
+                                        <input type="password" id="txtnpwd" class="form-control h-58 text-dark" name="new_password">
+                                        <i style="color: #A9A9C8; font-size: 16px; right: 15px !important;" class="ri-eye-off-line password-toggle-icon translate-middle-y top-50 end-0 position-absolute" aria-hidden="true"></i>
+                                        <small>Password must be 8 characters, with at least 1 number, 1 lowercase, and 1 uppercase letter.</small>
+                                        @error('new_password')
+                                        <div class="text-danger">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-12">
+                            <div class="form-group mb-4">
+                                <label class="label" for="txtcpwd">Confirm Password</label>
+                                <div class="form-group">
+                                    <div class="password-wrapper position-relative">
+                                        <input type="password" id="txtcpwd" class="form-control h-58 text-dark" name="confirm_password">
+                                        <i style="color: #A9A9C8; font-size: 16px; right: 15px !important;" class="ri-eye-off-line password-toggle-icon translate-middle-y top-50 end-0 position-absolute" aria-hidden="true"></i>
+                                        @error('confirm_password')
+                                        <div class="text-danger">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-12">
+                            <div class="form-group d-flex gap-3">
+                                <button class="btn btn-primary py-3 px-5 fw-semibold text-white" type="submit">Change Password</button>
+                            </div>
+                        </div>
+                    </div>     
+                </form>
+                  <!-- JavaScript to remove the success message after 10 seconds -->
         <script>
             document.addEventListener('DOMContentLoaded', function () {
                 const successAlert = document.getElementById('success-alert');
@@ -71,5 +108,8 @@
                 }
             });
         </script>
+            </div>
+        </div>
     </div>
+</div>
 @endsection
