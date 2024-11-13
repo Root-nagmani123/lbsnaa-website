cc5de6c8f0794538c995d43d8bed192fbb3f156e
nibhashlbsnaa <nibhash.mishra@awzpact.com>
2024-11-13 10:35:06 +0530

    tender ui changes

--- resources/views/admin/manage_tender/create.blade.php
+++ resources/views/admin/manage_tender/create.blade.php
@@ -2,17 +2,33 @@
 @section('title', 'Admin Dashboard')
 
 @section('content')
+<div class="d-sm-flex text-center justify-content-between align-items-center mb-4">
+    <h3 class="mb-sm-0 mb-1 fs-18">Manage Tender</h3>
+    <ul class="ps-0 mb-0 list-unstyled d-flex justify-content-center">
+        <li>
+            <a href="{{ route('admin.index') }}" class="text-decoration-none">
+                <i class="ri-home-2-line" style="position: relative; top: -1px;"></i>
+                <span>Dashboard</span>
+            </a>
+        </li>
+        <li>
+            <span class="fw-semibold fs-14 heading-font text-dark dot ms-2">Tender</span>
+        </li>
+    </ul>
+</div>
 <div class="row justify-content-center">
     <div class="col-lg-12">
         <div class="card bg-white border-0 rounded-10 mb-4">
             <div class="card-body p-4">
-                <h2>Add New Tender/Circular</h2>
+                <h4 class="fs-18 mb-4">Add New Tender/Circular</h4>
                 <form action="{{ route('manage_tender.store') }}" method="POST" enctype="multipart/form-data">
                     @csrf
                     @include('admin.manage_tender.form')
-                    <button type="submit" class="btn btn-success">Submit</button>
-                    <button type="reset" class="btn btn-secondary">Reset</button>
-                    <a href="{{ route('manage_tender.index') }}" class="btn btn-danger">Cancel</a>
+                    <div class="d-flex ms-sm-3 ms-md-0">
+                        <button class="btn btn-success text-white fw-semibold" type="submit">Submit</button>&nbsp;
+                        <button class="btn btn-secondary text-white fw-semibold" type="reset">Reset</button>&nbsp;
+                        <a href="{{ route('manage_tender.index') }}" class="btn btn-primary text-white fw-semibold">Cancel</a>
+                    </div>
                 </form>
             </div>
         </div>
--- resources/views/admin/manage_tender/edit.blade.php
+++ resources/views/admin/manage_tender/edit.blade.php
@@ -2,20 +2,32 @@
 @section('title', 'Admin Dashboard')
 
 @section('content')
+<div class="d-sm-flex text-center justify-content-between align-items-center mb-4">
+    <h3 class="mb-sm-0 mb-1 fs-18">Manage Tender</h3>
+    <ul class="ps-0 mb-0 list-unstyled d-flex justify-content-center">
+        <li>
+            <a href="{{ route('admin.index') }}" class="text-decoration-none">
+                <i class="ri-home-2-line" style="position: relative; top: -1px;"></i>
+                <span>Dashboard</span>
+            </a>
+        </li>
+        <li>
+            <span class="fw-semibold fs-14 heading-font text-dark dot ms-2">Tender</span>
+        </li>
+    </ul>
+</div>
 <div class="row justify-content-center">
     <div class="col-lg-12">
         <div class="card bg-white border-0 rounded-10 mb-4">
             <div class="card-body p-4">
-                <h2>Edit Tender/Circular</h2>
+                <h4 class="fs-18 mb-4">Edit Tender/Circular</h4>
                 <form action="{{ route('manage_tender.update', $manageTender->id) }}" method="POST" enctype="multipart/form-data">
                     @csrf
                     @method('PUT')
-                    <div class="row">
-                       @include('admin.manage_tender.form')
-                        <div class="d-flex ms-sm-3 ms-md-0">
-                            <button class="btn btn-success text-white fw-semibold" type="submit">Update Tender</button>&nbsp;
-                            <a href="{{ route('manage_tender.index') }}" class="btn btn-secondary text-white fw-semibold">Back</a>
-                        </div>
+                    @include('admin.manage_tender.form')
+                    <div class="d-flex ms-sm-3 ms-md-0">
+                        <button class="btn btn-success text-white fw-semibold" type="submit">Update Tender</button>&nbsp;
+                        <a href="{{ route('manage_tender.index') }}" class="btn btn-primary text-white fw-semibold">Back</a>
                     </div>
                 </form>
             </div>
