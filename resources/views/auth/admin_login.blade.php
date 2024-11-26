6451a03370ce061addd69bb06d7fcc1d89a72856
Root-nagmani123 <nagmani.singh@awzpact.com>
2024-11-26 12:26:24 +0530

    index page dynamic mayank

--- resources/views/auth/admin_login.blade.php
+++ resources/views/auth/admin_login.blade.php
@@ -6,18 +6,18 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="stylesheet" href="{{ asset('public/admin_assets/css/remixicon.css') }}">
-    <link rel="stylesheet" href="{{ asset('public/admin_assets/css/flaticon.css') }}">
-    <link rel="stylesheet" href="{{ asset('public/admin_assets/css/sidebar-menu.css') }}">
-    <link rel="stylesheet" href="{{ asset('public/admin_assets/css/simplebar.css') }}">
-    <link rel="stylesheet" href="{{ asset('public/admin_assets/css/apexcharts.css') }}">
-    <link rel="stylesheet" href="{{ asset('public/admin_assets/css/prism.css') }}">
-    <link rel="stylesheet" href="{{ asset('public/admin_assets/css/rangeslider.css') }}">
-    <link rel="stylesheet" href="{{ asset('public/admin_assets/css/sweetalert.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('public/admin_assets/css/quill.snow.css') }}">
-    <link rel="stylesheet" href="{{ asset('public/admin_assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin_assets/css/remixicon.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin_assets/css/flaticon.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin_assets/css/sidebar-menu.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin_assets/css/simplebar.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin_assets/css/apexcharts.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin_assets/css/prism.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin_assets/css/rangeslider.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin_assets/css/sweetalert.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin_assets/css/quill.snow.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin_assets/css/style.css') }}">
 
-    <link rel="icon" type="image/png" href="{{ asset('public/admin_assets/images/favicon.ico') }}">
+    <link rel="icon" type="image/png" href="{{ asset('admin_assets/images/favicon.ico') }}">
 
     <title>LogIn | Lal Bahadur Shastri National Academy of Administration</title>
 </head>
