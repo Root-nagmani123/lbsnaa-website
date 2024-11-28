394659b45a08e2e72e4167e6bc72339807dcb6ab
nibhashlbsnaa <nibhash.mishra@awzpact.com>
2024-11-28 11:57:09 +0530

    Reapply "toggle button chnage on menu module"
    This reverts commit 51d4b6d73765fb826befdf53cf020758f18f42e5.

--- app/Helpers/menu_helpers.php
+++ app/Helpers/menu_helpers.php
@@ -9,10 +9,11 @@ if (!function_exists('renderMenu')) {
     
         $output .= '<td>' . getMenuPosition($menu->txtpostion) . '</td>';
         $output .= '<td class="gap-3"><a href="' . route('admin.menus.edit', $menu->id) . '" class="btn bg-success text-white btn-sm">Edit</a> &nbsp;<a href="' . route('admin.menus.delete', $menu->id) . '" class="btn btn-sm btn-primary text-white">Delete</a></td>';
-        $checked = $menu->menu_status ? 'checked' : '';
         $output .= '<td><div class="form-check form-switch">
-            <input class="form-check-input status-toggle" type="checkbox" role="switch" data-id="' . $menu->id . '" ' . $checked . '>
+            <input class="form-check-input status-toggle" type="checkbox" role="switch" data-table="menus" 
+            data-column="menu_status" data-id="' . $menu->id . '" ' . ($menu->menu_status ? 'checked' : '') . '>
           </div></td>';
+        
         // Render child menus
         foreach ($menu->children as $child) {
             $output .= renderMenu($child, $indent . '--- '); // Increase indent for child items
