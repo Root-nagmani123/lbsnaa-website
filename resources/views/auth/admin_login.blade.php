04839429b0fc8d9b9b54e74bc19eba253cea32f0
Root-nagmani123 <nagmani.singh@awzpact.com>
2024-11-28 13:47:09 +0530

    conflict resolved

--- resources/views/auth/admin_login.blade.php
+++ resources/views/auth/admin_login.blade.php
@@ -6,18 +6,18 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="stylesheet" href="{{ asset('public/admin_assets/css/remixicon.css') }}">
-    <link rel="stylesheet" href="{{ asset('public/admin_assets/css/flaticon.css') }}">
-    <link rel="stylesheet" href="{{ asset('public/admin_assets/css/sidebar-menu.css') }}">
-    <link rel="stylesheet" href="{{ asset('public/admin_assets/css/simplebar.css') }}">
-    <link rel="stylesheet" href="{{ asset('public/admin_assets/css/apexcharts.css') }}">
-    <link rel="stylesheet" href="{{ asset('public/admin_assets/css/prism.css') }}">
-    <link rel="stylesheet" href="{{ asset('public/admin_assets/css/rangeslider.css') }}">
-    <link rel="stylesheet" href="{{ asset('public/admin_assets/css/sweetalert.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('public/admin_assets/css/quill.snow.css') }}">
-    <link rel="stylesheet" href="{{ asset('public/admin_assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin_assets/css/remixicon.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin_assets/css/flaticon.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin_assets/css/sidebar-menu.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin_assets/css/simplebar.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin_assets/css/apexcharts.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin_assets/css/prism.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin_assets/css/rangeslider.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin_assets/css/sweetalert.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin_assets/css/quill.snow.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin_assets/css/style.css') }}">
 
-    <link rel="icon" type="image/png" href="{{ asset('public/admin_assets/images/favicon.ico') }}">
+    <link rel="icon" type="image/png" href="{{ asset('admin_assets/images/favicon.ico') }}">
 
     <title>LogIn | Lal Bahadur Shastri National Academy of Administration</title>
 </head>
@@ -48,7 +48,7 @@
                     <div class="card bg-white border-0 rounded-10 mb-4" style="width: 500px;">
                         <div class="d-flex align-items-center gap-4 mb-3 justify-content-center border-bottom">
                             <a href="index.html">
-                                <img src="{{ asset('public/admin_assets/images/logo.png')}}" alt="logo" width="300"
+                                <img src="{{ asset('admin_assets/images/logo.png')}}" alt="logo" width="300"
                                     style="padding: 20px; text-align: center;" class="img-fluid">
                             </a>
                         </div>
@@ -80,19 +80,19 @@
     </div>
 
 
-    <script src="{{ asset('public/admin_assets/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('public/admin_assets/js/sidebar-menu.js') }}"></script>
-    <script src="{{ asset('public/admin_assets/js/dragdrop.js') }}"></script>
-    <script src="{{ asset('public/admin_assets/js/rangeslider.min.js') }}"></script>
-    <script src="{{ asset('public/admin_assets/js/sweetalert.js') }}"></script>
-    <script src="{{ asset('public/admin_assets/js/quill.min.js') }}"></script>
-    <script src="{{ asset('public/admin_assets/js/data-table.js') }}"></script>
-    <script src="{{ asset('public/admin_assets/js/prism.js') }}"></script>
-    <script src="{{ asset('public/admin_assets/js/clipboard.min.js') }}"></script>
-    <script src="{{ asset('public/admin_assets/js/feather.min.js') }}"></script>
-    <script src="{{ asset('public/admin_assets/js/simplebar.min.js') }}"></script>
-    <script src="{{ asset('public/admin_assets/js/fslightbox.js') }}"></script>
-    <script src="{{ asset('public/admin_assets/js/custom/custom.js') }}"></script>
+    <script src="{{ asset('admin_assets/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('admin_assets/js/sidebar-menu.js') }}"></script>
+    <script src="{{ asset('admin_assets/js/dragdrop.js') }}"></script>
+    <script src="{{ asset('admin_assets/js/rangeslider.min.js') }}"></script>
+    <script src="{{ asset('admin_assets/js/sweetalert.js') }}"></script>
+    <script src="{{ asset('admin_assets/js/quill.min.js') }}"></script>
+    <script src="{{ asset('admin_assets/js/data-table.js') }}"></script>
+    <script src="{{ asset('admin_assets/js/prism.js') }}"></script>
+    <script src="{{ asset('admin_assets/js/clipboard.min.js') }}"></script>
+    <script src="{{ asset('admin_assets/js/feather.min.js') }}"></script>
+    <script src="{{ asset('admin_assets/js/simplebar.min.js') }}"></script>
+    <script src="{{ asset('admin_assets/js/fslightbox.js') }}"></script>
+    <script src="{{ asset('admin_assets/js/custom/custom.js') }}"></script>
 </body>
 
 </html>
---
+++ resources/views/user/includes/header.blade.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="author" content="Codescandy">
+
+  <!-- Favicon -->
+  <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/favicon.ico') }}">
+
+  <!-- CSS Assets -->
+  <link href="{{ asset('assets/fonts/feather/feather.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/libs/bootstrap-icons/font/bootstrap-icons.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/libs/simplebar/dist/simplebar.min.css') }}" rel="stylesheet">
+  <link rel="stylesheet" href="{{ asset('assets/css/theme.min.css') }}">
+  <link rel="canonical" href="LBSNAA">
+  <link href="{{ asset('assets/libs/tiny-slider/dist/tiny-slider.css') }}" rel="stylesheet">
+  <link rel="stylesheet" href="{{ asset('assets/libs/glightbox/dist/css/glightbox.min.css') }}">
+  <link rel="icon" type="image/png" href="{{ asset('admin_assets/images/favicon.ico') }}">
+
+  <title>Home | Lal Bahadur Shastri National Academy of Administration</title>
+</head>
+<header>
+  <nav class="navbar navbar-expand-lg">
+    <div class="container px-0">
+    <a href="#" class="d-block text-decoration-none">
+            <img src="{{ asset('admin_assets/images/logo.png') }}" alt="logo-icon" style="width: 300px;">
+        </a>
+      <!-- Button -->
+
+      <!-- Collapse -->
+      <div class="collapse navbar-collapse" id="navbar-default">
+        <ul class="navbar-nav mx-auto">
+          @php
+          $menus = DB::table('menus')->where('menu_status',1)->where('is_deleted',0)->where('txtpostion',1)->where('parent_id', 0)->get();
+          @endphp
+
+          @foreach($menus as $menu)
+          @php
+          $submenus = DB::table('menus')->where('menu_status',1)->where('is_deleted',0)->where('txtpostion',1)->where('parent_id', $menu->id)->get();
+          @endphp
+          <li class="nav-item dropdown">
+            @if(count($submenus) > 0 )
+            <a class="nav-link {{count($submenus) > 0 ? 'dropdown-toggle' : ''}}" href="{{$menu->menutitle == 'Research Center' ? '#' :route('user.navigationpagesbyslug', $menu->menu_slug)}}" id="navbarListing"
+              data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{$menu->menutitle}}</a>
+              <ul class="dropdown-menu dropdown-menu-arrow dropdown-menu-end" aria-labelledby="navbarPages">
+                @foreach($submenus as $submenu)
+                @include('user.components.menu-item', ['menu' => $submenu])
+                @endforeach
+              </ul>
+            @else
+            <a class="nav-link {{count($submenus) > 0 ? 'dropdown-toggle' : ''}}" href="{{$menu->menutitle == 'Research Center' ? '#' :route('user.navigationpagesbyslug', $menu->menu_slug)}}" id="navbarListing">
+              {{$menu->menutitle}}</a>
+            @endif
+          </li>
+          @endforeach
+        </ul>
+      </div>
+    </div>
+  </nav>
+</header>
