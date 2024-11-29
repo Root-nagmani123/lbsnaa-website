1915efe23e456f7efc3c51149df04c393c9c77e4
nibhashlbsnaa <nibhash.mishra@awzpact.com>
2024-11-29 15:46:01 +0530

    bug fixes

--- resources/views/admin/manage_survey/index.blade.php
+++ resources/views/admin/manage_survey/index.blade.php
@@ -3,10 +3,24 @@
 @section('title', 'Admin Dashboard')
 
 @section('content')
+<div class="d-sm-flex text-center justify-content-between align-items-center mb-4">
+    <h3 class="mb-sm-0 mb-1 fs-18">Manage Survey</h3>
+    <ul class="ps-0 mb-0 list-unstyled d-flex justify-content-center">
+        <li>
+            <a href="{{ route('admin.index') }}" class="text-decoration-none">
+                <i class="ri-home-2-line" style="position: relative; top: -1px;"></i>
+                <span>Dashboard</span>
+            </a>
+        </li>
+        <li>
+            <span class="fw-semibold fs-14 heading-font text-dark dot ms-2">Survey</span>
+        </li>
+    </ul>
+</div>
 <div class="card bg-white border-0 rounded-10 mb-4">
     <div class="card-body p-4">
         <div class="d-sm-flex text-center justify-content-between align-items-center border-bottom pb-20 mb-20">
-            <h4 class="fw-semibold fs-18 mb-sm-0">Manage Survey</h4>
+            <h4 class="fw-semibold fs-18 mb-sm-0">Survey</h4>
             
             <a href="{{ route('survey.create') }}">
                 <button class="border-0 btn btn-success py-2 px-3 px-sm-4 text-white fs-14 fw-semibold rounded-3">
