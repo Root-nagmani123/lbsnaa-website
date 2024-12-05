e5dd0012ac0b4d0ee03f20c6d6a50c74b39fa77a
nibhashlbsnaa <nibhash.mishra@awzpact.com>
2024-12-05 13:12:59 +0530

    bug fixes and website ui chnages

--- resources/views/admin/manage_organisationchart/create.blade.php
+++ resources/views/admin/manage_organisationchart/create.blade.php
@@ -16,58 +16,60 @@
         </li>
     </ul>
 </div>
-    <div class="row justify-content-center">
-        <div class="col-lg-12">
-            <div class="card bg-white border-0 rounded-10 mb-4">
-                <div class="card-body p-4">
-                    <div class="d-flex justify-content-between align-items-center border-bottom pb-20 mb-20">
-                        <h4 class="fw-semibold fs-18 mb-0">Add Organization Structure</h4>
-                    </div>
-                    <form action="{{ route('organisation_chart.store') }}" method="post" enctype="multipart/form-data">
-                        @csrf
-                        <div class="row">
+<div class="row justify-content-center">
+    <div class="col-lg-12">
+        <div class="card bg-white border-0 rounded-10 mb-4">
+            <div class="card-body p-4">
+                <div class="d-flex justify-content-between align-items-center border-bottom pb-20 mb-20">
+                    <h4 class="fw-semibold fs-18 mb-0">Add Organization Structure</h4>
+                </div>
+                <form action="{{ route('organisation_chart.store') }}" method="post" enctype="multipart/form-data">
+                    @csrf
+                    <div class="row">
 
-                            <input type="hidden" name="parent_id" value="{{ $parent_id }}">
-                            <!-- Page Language -->
-                            <div class="col-lg-6">
-                                <div class="form-group mb-4">
-                                    <label class="label" for="menutitle">Page Language :</label>
-                                    <span class="star">*</span>
-                                    <div class="form-group position-relative">
-                                        <input type="radio" name="language" value="1"> English
-                                        <input type="radio" name="language" value="2"> Hindi
-                                    </div>
+                        <input type="hidden" name="parent_id" value="{{ $parent_id }}">
+                        <!-- Page Language -->
+                        <div class="col-lg-6">
+                            <div class="form-group mb-4">
+                                <label class="label" for="menutitle">Page Language :</label>
+                                <span class="star">*</span>
+                                <div class="form-group position-relative">
+                                    <input type="radio" name="language" value="1"> English
+                                    <input type="radio" name="language" value="2"> Hindi
                                 </div>
                             </div>
+                        </div>
 
-                            <!-- Select Parent Employee -->
-                            <div class="col-lg-6">
-                                <div class="form-group mb-4">
-                                    <label class="label" for="menutitle">Select Parent Employee :</label>
-                                    <span class="star">*</span>
-                                    <div class="form-group position-relative">
-                                        <select name="parentcategory" id="parentcategory" class="form-select form-control ps-5 h-58">
-                                            <option value="">Select Employee</option>
-                                            @foreach ($records as $record)
-                                                <option value={{ $record->id }}>{{ $record->name }}</option>
-                                            @endforeach
-                                        </select>
-                                    </div>
+                        <!-- Select Parent Employee -->
+                        <div class="col-lg-6">
+                            <div class="form-group mb-4">
+                                <label class="label" for="menutitle">Select Parent Employee :</label>
+                                <span class="star">*</span>
+                                <div class="form-group position-relative">
+                                    <select name="parentcategory" id="parentcategory"
+                                        class="form-select form-control ps-5 h-58">
+                                        <option value="">Select Employee</option>
+                                        @foreach ($records as $record)
+                                        <option value={{ $record->id }}>{{ $record->name }}</option>
+                                        @endforeach
+                                    </select>
                                 </div>
                             </div>
+                        </div>
 
-                            <!-- Select Employee with Autocomplete -->
-                            <div class="col-lg-6">
-                                <div class="form-group mb-4">
-                                    <label class="label" for="menutitle">Select Category :</label>
-                                    <span class="star">*</span>
-                                    <div class="form-group position-relative">
-                                        <input type="text" id="employee_name" name="employee_name" autocomplete="" class="form-control text-dark ps-5 h-58">
-                                    </div>
+                        <!-- Select Employee with Autocomplete -->
+                        <div class="col-lg-6">
+                            <div class="form-group mb-4">
+                                <label class="label" for="menutitle">Select Category :</label>
+                                <span class="star">*</span>
+                                <div class="form-group position-relative">
+                                    <input type="text" id="employee_name" name="employee_name" autocomplete=""
+                                        class="form-control text-dark ps-5 h-58">
                                 </div>
                             </div>
+                        </div>
 
-                            <!-- <div class="col-lg-6">
+                        <!-- <div class="col-lg-6">
                                 <div class="form-group mb-4">
                                     <label class="label">Description :</label>
                                     <span class="star">*</span>
@@ -77,43 +79,43 @@
                                 </div>
                             </div> -->
 
-                            <!-- Page Status -->
-                            <div class="col-lg-6">
-                                <div class="form-group mb-4">
-                                    <label class="label">Page Status:</label>
-                                    <select name="status" class="form-select form-control ps-5 h-58">
-                                        <option value="" class="text-dark" selected>Select</option>
-                                        <option value="1" class="text-dark">Active</option>
-                                        <option value="0" class="text-dark">Inactive</option>
-                                    </select>
-                                </div>
+                        <!-- Page Status -->
+                        <div class="col-lg-6">
+                            <div class="form-group mb-4">
+                                <label class="label">Page Status:</label>
+                                <select name="status" class="form-select form-control ps-5 h-58">
+                                    <option value="" class="text-dark" selected>Select</option>
+                                    <option value="1" class="text-dark">Active</option>
+                                    <option value="0" class="text-dark">Inactive</option>
+                                </select>
                             </div>
                         </div>
-                        <!-- Submit Button -->
-                        <div class="d-flex ms-sm-3 ms-md-0">
-                            <button class="btn btn-success text-white fw-semibold" type="submit">Submit</button>&nbsp;
-                            <a href="{{ route('organisation_chart.index') }}" class="btn btn-secondary text-white">Cancel</a>
-                        </div>
-                </div>
-
-                </form>
+                    </div>
+                    <!-- Submit Button -->
+                    <div class="d-flex ms-sm-3 ms-md-0">
+                        <button class="btn btn-success text-white fw-semibold" type="submit">Submit</button>&nbsp;
+                        <a href="{{ route('organisation_chart.index') }}"
+                            class="btn btn-secondary text-white">Cancel</a>
+                    </div>
             </div>
+
+            </form>
         </div>
     </div>
-    </div>
-    <!-- jQuery and jQuery UI for Autocomplete -->
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css">
-    <script src="//code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="//code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+</div>
+<!-- jQuery and jQuery UI for Autocomplete -->
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css">
+<script src="//code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="//code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
 
-    <!-- Autocomplete Script -->
-    <script>
-        $(function() {
-            $("#employee_name").autocomplete({
-                source: "{{ route('employee.autocomplete') }}",
-                minLength: 2
-            });
-        });
-    </script>
+<!-- Autocomplete Script -->
+<script>
+$(function() {
+    $("#employee_name").autocomplete({
+        source: "{{ route('employee.autocomplete') }}",
+        minLength: 2
+    });
+});
+</script>
 
 @endsection
