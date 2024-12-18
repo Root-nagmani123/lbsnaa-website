8a4e7d49785606ee7f057cb72856983b8373aa19
nibhashlbsnaa <nibhash.mishra@awzpact.com>
2024-12-18 10:57:42 +0530

    ui chnages

--- resources/views/user/pages/mediagallery.blade.php
+++ resources/views/user/pages/mediagallery.blade.php
@@ -7,13 +7,12 @@
             <div class="col-md-12 col-lg-5">
                 <div class="mb-2">
                     <!-- title -->
-                    <h1 class="display-4 mb-3 fw-bold">Media Gallery</h1>
+                    <h3 class="mb-3 fw-bold">Media Gallery</h3>
                     <!-- text -->
 
                 </div>
             </div>
         </div>
-        <hr class="my-4">
         <!-- form -->
         @if(session('success'))
         <div class="alert alert-success alert-dismissible fade show" role="alert">
@@ -21,19 +20,32 @@
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
         @endif
-
-        <div class="row">
-            <div class="col-lg-4 col-md-4 col-12">
-                <div class="mb-4">
-                    <!-- text -->
-                    <p>here icon of Media Gallery</p>
-                    <a href="" class="btn btn-primary">Academy Song</a>
-                    <a href="{{ route('user.photogallery')}}" class="btn btn-primary">Photos Gallery</a>
-
-
+        <div class="container">
+            <div class="position-relative d-flex overflow-x-hidden py-lg-4 pt-4">
+                <div class="d-flex gap-3">
+                    <a href="#" class="bg-white text-center shadow-sm text-wrap rounded-4 w-100 border card-lift border"
+                        style="width: 200px !important">
+                        <div class="p-3">
+                            <img src="{{ asset('assets/images/audio-book.png') }}" alt="mentor 19"
+                                class="avatar avatar-xl">
+                        </div>
+                        <div class="mt-3">
+                            <h3 class="text-center">Academy Song</h3>
+                        </div>
+                    </a>
+                    <a href="{{ route('user.photogallery')}}"
+                        class="bg-white text-center shadow-sm text-wrap rounded-4 w-100 border card-lift border"
+                        style="width: 200px !important">
+                        <div class="p-3">
+                            <img src="{{ asset('assets/images/gallery (1).png') }}" alt="mentor 19"
+                                class="avatar avatar-xl">
+                        </div>
+                        <div class="mt-3">
+                            <h3 class="text-center">Photos Gallery</h3>
+                        </div>
+                    </a>
                 </div>
             </div>
-
         </div>
     </div>
 </section>
