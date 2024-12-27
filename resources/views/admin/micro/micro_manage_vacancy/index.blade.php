14336770f47df51680d3455022abcd861644e67e
Indradev8982 <indrajeet.kumar@awzpact.com>
2024-12-27 12:49:45 +0530

    Bug Fixes

--- resources/views/admin/micro/micro_manage_vacancy/index.blade.php
+++ resources/views/admin/micro/micro_manage_vacancy/index.blade.php
@@ -47,6 +47,11 @@
                 {{ session('error') }}
             </div>
         @endif
+        @if (session('error'))
+            <div class="alert alert-danger">
+                {{ session('error') }}
+            </div>
+        @endif
         <div class="default-table-area members-list">
             <div class="table-responsive">
                 <table class="table align-middle" id="myTable">
