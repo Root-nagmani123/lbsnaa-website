dc0714ee8b20f6b5b1bc269bbb897e6747195254
nibhashlbsnaa <nibhash.mishra@awzpact.com>
2025-04-04 17:01:09 +0530

    download button

--- resources/views/user/pages/vacancy.blade.php
+++ resources/views/user/pages/vacancy.blade.php
@@ -97,7 +97,11 @@
                         </td>
                             <td>
                                 @if(!empty($value->document_upload))
-                                <a href="{{ asset('storage/' . $value->document_upload) }}" target="_blank">Download</a>
+                                <a href="{{ asset('storage/' . $value->document_upload) }}" target="_blank">@if($_COOKIE['language'] ==
+                            '2')डाउनलोड
+                            @else
+                            Download
+                            @endif</a>
                                 @else
                                 N/A
                                 @endif
